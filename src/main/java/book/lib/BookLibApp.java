9a0b5cc57d86a0cac1f2f978431086f9678b546d
ViLevin <vilevinqa@gmail.com>
2025-05-17 10:44:04 +0300

    initial commit

---
+++ src/main/java/book/lib/BookLibApp.java
@@ -0,0 +1,13 @@
+package book.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLibApp.class, args);
+	}
+
+}
