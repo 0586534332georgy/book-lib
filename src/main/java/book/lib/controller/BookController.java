cc4095f2e4095d7ddf31ec572c31ece8f88cb129
ViLevin <vilevinqa@gmail.com>
2025-05-24 08:56:31 +0300

    Service realizes

--- src/main/java/book/lib/controller/BookController.java
+++ src/main/java/book/lib/controller/BookController.java
@@ -2,7 +2,6 @@ package book.lib.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,83 +13,64 @@ import book.lib.dto.BookCredentialsDto;
 import book.lib.dto.BookDto;
 import book.lib.dto.BookReservedDto;
 import book.lib.entity.Book;
-import book.lib.repo.BookRepository;
-import book.lib.repo.StatusRepository;
+import book.lib.service.BookService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class BookController {
 
-	@Autowired
-	private BookRepository bookRepo;
-	
-	@Autowired
-	private StatusRepository statusRepo;
+	private final BookService bookService;
 
 	@GetMapping("/books")
 	public List<Book> getAll() {
-		return bookRepo.findAll();
+		return bookService.getAll();
 	}
-	
+
 	@GetMapping("/books-dto")
 	public List<BookDto> getAll2() {
-		return bookRepo.findAllBooks();
+		return bookService.getAll2();
 	}
-	
 
 //	http://localhost:8080/api/books-by-genre?bookGenre=Fantasy
-    @GetMapping("/books-by-genre")
-    public List<BookCredentialsDto> getBooksByGenre(@RequestParam BookGenreEnum bookGenre) {
-    	String dbGenre = bookGenre.name();
-        return bookRepo.findByBookGenre(dbGenre);
-    }
+	@GetMapping("/books-by-genre")
+	public List<BookCredentialsDto> getBooksByGenre(@RequestParam BookGenreEnum bookGenre) {
+		return bookService.getBooksByGenre(bookGenre);
+	}
 
-    
 //  http://localhost:8080/api/books-by-genre-and-pages?genre=Horror&min=200&max=400
-    @GetMapping("/books-by-genre-and-pages")
-    public List<BookCredentialsDto> getBooksByGenreAndPages (
-    		@RequestParam BookGenreEnum genre,
-    		@RequestParam int min,
-    		@RequestParam int max
-    		) 
-    	{
-    	String dbGenre = genre.name();
-    	return bookRepo.findByBookGenreAndPagesAmountBetween(dbGenre, min, max);
-    }
-    
+	@GetMapping("/books-by-genre-and-pages")
+	public List<BookCredentialsDto> getBooksByGenreAndPages(@RequestParam BookGenreEnum genre, @RequestParam int min,
+			@RequestParam int max) {
+		return bookService.getBooksByGenreAndPages(genre, min, max);
+	}
+
 //  http://localhost:8080/api/books-by-genre-and-pages-ordered?genre=Horror&min=200&max=400    
-    @GetMapping("/books-by-genre-and-pages-ordered")
-    public List<BookCredentialsDto> getBooksByGenreAndPagesOrdered (
-    		@RequestParam BookGenreEnum genre,
-    		@RequestParam int min,
-    		@RequestParam int max
-    		) 
-    	{
-    	String dbGenre = genre.name();
-    	return bookRepo.findByBookGenreAndPagesAmountBetweenOrderByPagesAmountAsc(dbGenre, min, max);
-    }
-    
-    @GetMapping("/books-free")
-    public List<BookDto> getFreeBooks() {
-    	return statusRepo.getFreeBooks();
-    }
-    
-    @GetMapping("/books-reserved")
-    public List<BookReservedDto> getReservedBooks() {
-    	return statusRepo.getReservedBooks();
-    }
-    
-    @PutMapping("/free-book")
-    public int setBookFree(@RequestParam String title) {
-    	return statusRepo.setBookFree(title);
-    	
-    }
-    
-    @PutMapping("/reserve-book")
-    public int setBookReserved(@RequestParam String title) {
-    	return statusRepo.setBookReserved(title);
-    	
-    }
-    
-    
+	@GetMapping("/books-by-genre-and-pages-ordered")
+	public List<BookCredentialsDto> getBooksByGenreAndPagesOrdered(@RequestParam BookGenreEnum genre,
+			@RequestParam int min, @RequestParam int max) {
+		return bookService.getBooksByGenreAndPagesOrdered(genre, min, max);
+	}
+
+	@GetMapping("/books-free")
+	public List<BookDto> getFreeBooks() {
+		return bookService.getFreeBooks();
+	}
+
+	@GetMapping("/books-reserved")
+	public List<BookReservedDto> getReservedBooks() {
+		return bookService.getReservedBooks();
+	}
+
+	@PutMapping("/free-book")
+	public int setBookFree(@RequestParam String title) {
+		return bookService.setBookFree(title);
+	}
+
+	@PutMapping("/reserve-book")
+	public int setBookReserved(@RequestParam String title) {
+		return bookService.setBookReserved(title);
+	}
+
 }
---
+++ src/main/java/book/lib/service/BookServiceImpl.java
@@ -0,0 +1,64 @@
+package book.lib.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import book.lib.api.BookGenreEnum;
+import book.lib.dto.BookCredentialsDto;
+import book.lib.dto.BookDto;
+import book.lib.dto.BookReservedDto;
+import book.lib.entity.Book;
+import book.lib.repo.BookRepository;
+import book.lib.repo.StatusRepository;
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+	final private BookRepository bookRepo;
+
+	final private StatusRepository statusRepo;
+
+	public List<Book> getAll() {
+		return bookRepo.findAll();
+	}
+
+	public List<BookDto> getAll2() {
+		return bookRepo.findAllBooks();
+	}
+
+	public List<BookCredentialsDto> getBooksByGenre(BookGenreEnum bookGenre) {
+		String dbGenre = bookGenre.name();
+		return bookRepo.findByBookGenre(dbGenre);
+	}
+
+	public List<BookCredentialsDto> getBooksByGenreAndPages(BookGenreEnum genre, int min, int max) {
+		String dbGenre = genre.name();
+		return bookRepo.findByBookGenreAndPagesAmountBetween(dbGenre, min, max);
+	}
+
+	public List<BookCredentialsDto> getBooksByGenreAndPagesOrdered(BookGenreEnum genre, int min, int max) {
+		String dbGenre = genre.name();
+		return bookRepo.findByBookGenreAndPagesAmountBetweenOrderByPagesAmountAsc(dbGenre, min, max);
+	}
+
+	public List<BookDto> getFreeBooks() {
+		return statusRepo.getFreeBooks();
+	}
+
+	public List<BookReservedDto> getReservedBooks() {
+		return statusRepo.getReservedBooks();
+	}
+
+	public int setBookFree(String title) {
+		return statusRepo.setBookFree(title);
+	}
+
+	public int setBookReserved(String title) {
+		return statusRepo.setBookReserved(title);
+	}
+
+}
